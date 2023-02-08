271b63541dcddc8828fb28dd84e2245b1aebce2d
Ankur Singhal <ankurrsinghal@gmail.com>
2023-02-08 06:30:31 +0530

    updated the readme

--- docs/components/AutoResizeTextarea.vue
+++ docs/components/AutoResizeTextarea.vue
@@ -1,17 +1,17 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import AutoResizeTextarea from 'vue3-autoresize-textarea';
+import AutoResizeTextarea from "vue3-autoresize-textarea";
 
 let node = ref(null);
-
 </script>
 
 <template>
-<AutoResizeTextarea v-bind="value" ref="node" title="HELLLO" />
+  <AutoResizeTextarea v-bind="value" ref="node" title="HELLLO" />
 </template>
 
 <style>
 textarea {
-    border: 1px solid black;
+  border: 2px solid black;
+  padding: 10px;
 }
 </style>
